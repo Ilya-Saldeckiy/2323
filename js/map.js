0a97895141708a042abe782a7fa36f94a9b7d440
Ilya-Saldeckiy <melllllowtoy@yandex.ru>
2021-09-28 12:53:28 +0300

    fix

--- js/map.js
+++ js/map.js
@@ -1,108 +1,48 @@
-ymaps.ready(function () {
- 
-  // Поиск координатов: https://yandex.ru/map-constructor/location-tool/
- 
-  var myMap = new ymaps.Map('map-1', {
-          // Задаем центр карты
-          center: [59.94267813289089,30.316415999999972],
-          // И масштаб
-          zoom: 15
-      }, {
-          searchControlProvider: 'yandex#search'
-      }),
- 
-      // Создаём макет содержимого.
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-      );
-    
- 
- 
-  var createPlacemark = function(markerId, coord_1, coord_2, markerImage, name, addr) {
- 
-    markerId = new ymaps.GeoObject({
-      geometry: {
-        type: "Point",
-        coordinates: [+coord_1, +coord_2]
-      },
-      properties: {
-        hintContent: name,
-        balloonContent: addr
-      }
-    }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: markerImage,
-        // Размеры метки.
-        iconImageSize: [46, 46],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-20, -45]
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.adress-btn').forEach(function(tabs){
+        tabs.addEventListener('click', function(event){
+            const path = event.currentTarget.dataset.path
+            console.log(path)
+
+            document.querySelectorAll('.map-position').forEach(function(tabContent) {
+                tabContent.classList.remove('map-active')
+            })
+            document.querySelector(`[data-target='${path}']`).classList.add('map-active')
+        })
+    })
+})
+ymaps.ready(init);
+function init(){
+    var myMap = new ymaps.Map("mapFirst", {
+        center: [55.75846306898368,37.601079499999905],
+        zoom: 15
     });
- 
-    myMap.geoObjects.add(markerId);
-  };
- 
-  // Добавление маркеров на карту
-  createPlacemark('myPlacemark_1', '59.94267813289089', '30.316415999999972', 'img/map_marker_blue.svg', 'Место силы', 'Санкт-Петербург, Эрмитажная пристань ');
-  createPlacemark('myPlacemark_2', '59.93545363045839', '30.326887343994116', 'img/map_marker_red.svg', 'Квест 1', 'Вход в метро');
-  createPlacemark('myPlacemark_3', '59.93598684451666', '30.314752621881883', 'img/map_marker_green.svg', 'Квест 2', 'Вход в метро');
- 
-});
+    var myMapSecond = new ymaps.Map("mapSecond", {
+        center: [55.75846306898368,37.601079499999905],
+        zoom: 15
+    });
+
+}
+
+$('.adress-btn').click(function(){
+    $(this).toggleClass('btn-active');
+    $('.adress-btn').not(this).removeClass('btn-active');
+})
+
+ymaps.ready(function () {
+
+    var myMap = new ymaps.Map("map-1", {
+        center: [55.75846306898368, 37.601079499999905],
+        zoom: 15
+    });
+})
 
 
 ymaps.ready(function () {
- 
-  // Поиск координатов: https://yandex.ru/map-constructor/location-tool/
- 
-  var myMap = new ymaps.Map('map-2', {
-          // Задаем центр карты
-          center: [59.94267813289089,30.316415999999972],
-          // И масштаб
-          zoom: 15
-      }, {
-          searchControlProvider: 'yandex#search'
-      }),
- 
-      // Создаём макет содержимого.
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-      );
-    
- 
- 
-  var createPlacemark = function(markerId, coord_1, coord_2, markerImage, name, addr) {
- 
-    markerId = new ymaps.GeoObject({
-      geometry: {
-        type: "Point",
-        coordinates: [+coord_1, +coord_2]
-      },
-      properties: {
-        hintContent: name,
-        balloonContent: addr
-      }
-    }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: markerImage,
-        // Размеры метки.
-        iconImageSize: [46, 46],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-5, -38]
+
+    var myMap = new ymaps.Map("map-2", {
+        center: [55.75846306898368, 37.601079499999905],
+        zoom: 15
     });
- 
-    myMap.geoObjects.add(markerId);
-  };
- 
-  // Добавление маркеров на карту
-  createPlacemark('myPlacemark_1', '59.94267813289089', '30.316415999999972', 'img/map_marker_blue.svg', 'Место силы', 'Санкт-Петербург, Эрмитажная пристань ');
-  createPlacemark('myPlacemark_2', '59.93545363045839', '30.326887343994116', 'img/map_marker_red.svg', 'Квест 1', 'Вход в метро');
-  createPlacemark('myPlacemark_3', '59.93598684451666', '30.314752621881883', 'img/map_marker_green.svg', 'Квест 2', 'Вход в метро');
- 
 });
